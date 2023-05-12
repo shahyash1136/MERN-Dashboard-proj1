088775fcde4cbaa3efa19c81cc47ae4cc60d480d
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2023-05-13 02:20:42 +0530

    basic project setup

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* Configuration */
+dotenv.config("./env");
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* Routes */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+/* Mongoose Setup */
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB Connection Successfully");
+  })
+  .catch((err) => {
+    console.log(`DB connection error: ${err}`);
+  });
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
